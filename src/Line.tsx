9b8fc2f8435cf40e1bfdc8bc9ebe04dcbed24990
Vasiliy Kryuchkov <soundbylp@gmail.com>
2020-09-10 14:56:39 +0300

    add points on Line

--- src/Line.tsx
+++ src/Line.tsx
@@ -1,6 +1,6 @@
 import deepmerge from 'deepmerge'
 import * as React from 'react'
-import { Path } from 'react-native-svg'
+import { Path, Circle } from 'react-native-svg'
 
 import ChartContext from './ChartContext'
 import { adjustPointsForThickStroke, calculateTooltipIndex } from './Line.utils'
@@ -25,7 +25,7 @@ const Line: React.FC<Props> = (props) => {
   const { data: contextData, dimensions, domain, lastTouch } = React.useContext(ChartContext)
 
   const {
-    theme: { stroke },
+    theme: { stroke, points },
     tooltipComponent,
     data = contextData,
     tension,
@@ -47,6 +47,10 @@ const Line: React.FC<Props> = (props) => {
   return (
     <React.Fragment>
       <Path d={path} fill="none" strokeLinecap="round" stroke={stroke.color} strokeWidth={stroke.width} strokeOpacity={stroke.opacity}></Path>
+      {pointsWithinDimensions.map((p, index) => (
+        <Circle key={`x${p.x}, y${p.y}`} cx={p.x} cy={p.y} r={points.radius} fill={tooltipIndex === index ? points.selectedColor : points.color} />
+      ))}
+
       {tooltipIndex !== undefined &&
         tooltipComponent &&
         React.cloneElement(tooltipComponent, { value: data[tooltipIndex], position: scaledPoints[tooltipIndex] })}
@@ -58,6 +62,11 @@ export { Line }
 
 const defaultProps = {
   theme: {
+    points: {
+      color: 'black',
+      selectedColor: 'red',
+      radius: 5,
+    },
     stroke: {
       color: 'black',
       width: 1,
